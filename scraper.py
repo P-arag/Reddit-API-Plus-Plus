96638b3f21de945f0b93b972e98b933e6b329a34
Parag Jyoti Paul <68776840+P-arag@users.noreply.github.com>
2020-11-29 17:05:47 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,11 +6,11 @@ def get_data(subreddit_name):
 
     dataArr = []
     commentArr = []
-    reddit = praw.Reddit(client_id="6DK_x8Hhm5NHSg",
-                         client_secret="e1-6EmPVCmnmlOFbc39_lSZMQ5_pOw",
-                         password="parag1Jyoti",
-                         user_agent="dailydoseofmemes",
-                         username="ScorpoRio")
+    reddit = praw.Reddit(client_id="YOUR_ID",
+                         client_secret="YOUR_SECRET",
+                         password="YOUR_EMAIL_PASSWORD",
+                         user_agent="NAME_OF_THE_APP",
+                         username="REDDIT_USERNAME")
 
     subreddit = reddit.subreddit(subreddit_name)
     members = subreddit.subscribers
